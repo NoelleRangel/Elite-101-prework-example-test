2b5b4a10bf16ea62f0955119fe5f437c8aa4301a
Noelle Rangel <192288701+NoelleRangel@users.noreply.github.com>
2025-01-06 20:59:06 +0000

    updates: added fav_color variable which asks for user input.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
 print('Welcome to the food ordering chatbot')
 name = input('What is your name? ')
 age = int(input('What is your age? '))
+fav_color = input(f'Hello {name}, what is your favorite color? ')
